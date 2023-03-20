fd3069ad53a81fa5b0ca4d83fe560ead69972ee0
jck1044 <jck1044@wildcats.unh.edu>
2023-03-20 11:22:08 -0400

    simplify stm code

--- fprime/fprime-feat-osal/Os/stm32-bsp/Core/Src/main.c
+++ fprime/fprime-feat-osal/Os/stm32-bsp/Core/Src/main.c
@@ -131,6 +131,7 @@ PUTCHAR_PROTOTYPE
   HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
   return ch;
 }
+
 /* USER CODE END 0 */
 
 /**
@@ -190,19 +191,18 @@ int main(void)
   /* USER CODE BEGIN RTOS_TIMERS */
   //  	HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_4);
   // system("st-flash write remakeFPrime.bin 0x8000000");
-  for (int i = 0; i < 20; i++)
-  {
-    printf("\n\rXXXXX1\n\r");
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
-    HAL_Delay(500);
-  }
+//  for (int i = 0; i < 20; i++)
+//  {
+////    printf("\n\rXXXXX1\n\r");
+//    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+//    HAL_Delay(500);
+//  }
 
   /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
-  
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
@@ -210,6 +210,13 @@ int main(void)
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
+  fsw_main();
+  for (int i = 0; i < 20; i++)
+  {
+//    printf("\n\rXXXXX2\n\r");
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+    HAL_Delay(500);
+  }
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
 
@@ -1096,13 +1103,12 @@ void StartDefaultTask(void *argument)
 {
   // system("st-flash write remakeFPrime.bin 0x8000000");
   /* USER CODE BEGIN 5 */
-  for (int i = 0; i < 20; i++)
-  {
-    printf("\n\rXXXXX2\n\r");
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
-    HAL_Delay(500);
-  }
-  fsw_main();
+//  for (int i = 0; i < 20; i++)
+//  {
+////    printf("\n\rXXXXX2\n\r");
+//    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+//    HAL_Delay(500);
+//  }
   /* Infinite loop */
   for (;;)
   {
